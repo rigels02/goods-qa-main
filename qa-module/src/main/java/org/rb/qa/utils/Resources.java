601778561c997196edf0fd37bfc0d5cb3d53e686
Raitis <rigels02@gmail.com>
2018-06-03 18:27:25 +0300

    As data resources are growing (several files) and to make data modular the data resources are removed from qa-module to separate module qa-data.

--- qa-module/src/main/java/org/rb/qa/utils/Resources.java
+++ qa-module/src/main/java/org/rb/qa/utils/Resources.java
@@ -17,37 +17,41 @@ import java.util.List;
  */
 public class Resources {
 
-   public  List<String> getResourceFiles( String path ) throws IOException {
-    List<String> filenames = new ArrayList<>();
+    public List<String> getResourceFiles(String path) throws IOException {
+        List<String> filenames = new ArrayList<>();
 
-    try(
-      InputStream in = getResourceAsStream( path );
-      BufferedReader br = new BufferedReader( new InputStreamReader( in ) ) ) {
-      String resource;
+        try (
+                InputStream in = getResourceAsStream(path);
+                BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
+            String resource;
 
-      while( (resource = br.readLine()) != null ) {
-        filenames.add( resource );
-      }
+            while ((resource = br.readLine()) != null) {
+                filenames.add(resource);
+            }
+        }
+
+        return filenames;
     }
-    
-    return filenames;
-  }
-    
-    public InputStream getResourceAsStream( String resource ) {
-    
-    return getClass().getResourceAsStream( resource );
-  }
-    public void copyResourceToLocal(String sourceFile) throws IOException{
-       if(!isAndroid()) {
-       Path path = Paths.get(sourceFile).getFileName();
-    //InputStream ins = getResourceAsStream("/assetsj/xmlTest_1.xml");
-    InputStream ins = getResourceAsStream(sourceFile);
-        //Path path = Paths.get("xmlEx.xml");
-        Files.copy(ins, path);
-       } else {
-           
-        throw new UnsupportedOperationException("Not supported yet.");
-       }
+
+    public InputStream getResourceAsStream(String resource) {
+
+        return getClass().getResourceAsStream(resource);
+    }
+
+    public void copyResourceToLocal(String sourceFile) throws IOException {
+        if (!isAndroid()) {
+            Path path = Paths.get(sourceFile).getFileName();
+            //InputStream ins = getResourceAsStream("/assetsj/xmlTest_1.xml");
+            InputStream ins = getResourceAsStream(sourceFile);
+            if (ins == null) {
+                throw new IOException("Resource/data file not available: " + sourceFile);
+            }
+            //Path path = Paths.get("xmlEx.xml");
+            Files.copy(ins, path);
+        } else {
+
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
     }
 
     /**
@@ -55,7 +59,7 @@ public class Resources {
      * <h2>Description:</h2>
      * <pre>
      *  <b>if resources directory have the following structure: </b>
-     * 
+     *
      *   ──assetsj
      *  │   assets-list.txt
      *  │   knb.xml
@@ -64,7 +68,7 @@ public class Resources {
      *  └───images
      *           icons8-html-48.png
      * </pre>
-     *  <b> Then assets-list.txt should have lines:</b><br>
+     * <b> Then assets-list.txt should have lines:</b><br>
      * <pre>
      * images/icons8-html-48.png
      * knb.xml
@@ -72,19 +76,20 @@ public class Resources {
      * xmlTest.xml
      * </pre>
      * <hr>
-     * @param sourceAssetsDir source resources directory in 'resource' directory, 
-     * must be with leading and ending '/' (ex.: /assetsj/).
-     * @param targetDir target directory if any and  without leading '/' 
-     * to make it in application directory
-     * @throws IOException 
+     *
+     * @param sourceAssetsDir source resources directory in 'resource'
+     * directory, must be with leading and ending '/' (ex.: /assetsj/).
+     * @param targetDir target directory if any and without leading '/' to make
+     * it in application directory
+     * @throws IOException
      */
-    public void copyResourceToLocalByListFile(String sourceAssetsDir,String targetDir) throws IOException {
-        
+    public void copyResourceToLocalByListFile(String sourceAssetsDir, String targetDir) throws IOException {
+
         //put target directory without leading '/' to make it in application directory
         //leave as empty string if target directory is not needed
-        Path targetAssetDir = Paths.get((targetDir==null)?"":targetDir); 
+        Path targetAssetDir = Paths.get((targetDir == null) ? "" : targetDir);
         //ex. /assetsj/assets-list.txt
-        String resourceListFile= sourceAssetsDir+"assets-list.txt";
+        String resourceListFile = sourceAssetsDir + "assets-list.txt";
         try (BufferedReader listFile = new BufferedReader(
                 new InputStreamReader(
                         getClass().getResourceAsStream(resourceListFile),
@@ -93,10 +98,10 @@ public class Resources {
             String assetResource;
             while ((assetResource = listFile.readLine()) != null) {
                 Path assetFile = targetAssetDir.resolve(assetResource);
-                if(assetFile.getParent()!=null){
-                Path dir = Files.createDirectories(assetFile.getParent());
+                if (assetFile.getParent() != null) {
+                    Path dir = Files.createDirectories(assetFile.getParent());
                 }
-                try (InputStream asset = getClass().getResourceAsStream(sourceAssetsDir+assetResource)) {
+                try (InputStream asset = getClass().getResourceAsStream(sourceAssetsDir + assetResource)) {
                     Files.copy(asset, assetFile);
                 }
             }
@@ -106,5 +111,5 @@ public class Resources {
     private boolean isAndroid() {
         return System.getProperty("java.vm.name").equalsIgnoreCase("Dalvik");
     }
-    
+
 }
